03248bcab187ec570930230cc43e482caaad18c7
Kidong.Cheon <k.cheon@samsung.com>
2024-06-18 13:57:57 +0900

    [Feature] Read 수행시 read cmd ssd로 전달하는           함수 호출 확인

--- shell_pkg/shell.py
+++ shell_pkg/shell.py
@@ -1,3 +1,10 @@
 class Shell:
     def __init__(self):
         pass
+
+    def read(self, param):
+        self.send_cmd_to_ssd()
+        pass
+
+    def send_cmd_to_ssd(self):
+        pass
--- shell_pkg/test_shell.py
+++ shell_pkg/test_shell.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from unittest.mock import Mock
+
+from shell_pkg.shell import Shell
 
 
 class TestShell(TestCase):
     def test_placeholder(self):
         self.assertEqual(1, 1)
+
+    def test_read_calling_send_cmd_to_ssd(self):
+        shell = Shell()
+        shell.send_cmd_to_ssd = Mock()
+        shell.read(3)
+        self.assertEqual(shell.send_cmd_to_ssd.call_count, 1)
