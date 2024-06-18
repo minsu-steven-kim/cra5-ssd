7e05b7b2c75be54596df7b18f19b32234b0ccab2
Kidong.Cheon <k.cheon@samsung.com>
2024-06-18 14:00:46 +0900

    [Refactor] Testcase 중복코드 제거

--- shell_pkg/test_shell.py
+++ shell_pkg/test_shell.py
@@ -5,17 +5,18 @@ from shell_pkg.shell import Shell
 
 
 class TestShell(TestCase):
+    def setUp(self):
+        self.shell = Shell()
+
     def test_placeholder(self):
         self.assertEqual(1, 1)
 
     def test_read_calling_send_cmd_to_ssd(self):
-        shell = Shell()
-        shell.send_cmd_to_ssd = Mock()
-        shell.read(3)
-        self.assertEqual(shell.send_cmd_to_ssd.call_count, 1)
+        self.shell.send_cmd_to_ssd = Mock()
+        self.shell.read(3)
+        self.assertEqual(self.shell.send_cmd_to_ssd.call_count, 1)
 
     def test_read_calling_get_result_with_ssd(self):
-        shell = Shell()
-        shell.get_result_with_ssd = Mock()
-        shell.read(3)
-        self.assertEqual(shell.get_result_with_ssd.call_count, 1)
+        self.shell.get_result_with_ssd = Mock()
+        self.shell.read(3)
+        self.assertEqual(self.shell.get_result_with_ssd.call_count, 1)
