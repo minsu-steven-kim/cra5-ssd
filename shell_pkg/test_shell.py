0cceb9bd86339f9ff3959b7576d199d88366f26a
km4.kim <kyoomok@naver.com>
2024-06-18 14:30:22 +0900

    [refactoring] base 코드 삭제

--- shell_pkg/test_shell.py
+++ shell_pkg/test_shell.py
@@ -4,9 +4,6 @@ from shell import Shell
 
 
 class TestShell(TestCase):
-    def test_placeholder(self):
-        self.assertEqual(1, 1)
-
     def test_write_temporary(self):
         vs = Shell()
         vs.write(1, 0x12341234)
