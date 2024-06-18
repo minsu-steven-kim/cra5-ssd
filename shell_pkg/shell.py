4a3be449f7888ded0811d4ca9f375dfa427c15a5
km4.kim <kyoomok@naver.com>
2024-06-18 13:30:31 +0900

    [feature] 쓰기 값 type 검사 코드 추가

--- shell_pkg/shell.py
+++ shell_pkg/shell.py
@@ -8,8 +8,11 @@ class Shell:
     def write(self, lba: int, value: int):
         if self.is_invalid_lba(lba):
             raise Exception("INVALID COMMAND")
+        if type(value) != int:
+            raise Exception("INVALID COMMAND")
         if value < 0x00000000 or value > 0xFFFFFFFF:
             raise Exception("INVALID COMMAND")
+
 
     def is_invalid_lba(self, lba):
         if type(lba) != int:
