6f62645ae360e6618ece9e4ec279c71a8fd3759f
jihye <jhlovv08@gmail.com>
2024-06-18 14:28:55 +0900

    [refactor] extract method (is_valid_w/r_command)

--- virtual_ssd_pkg/virtual_ssd.py
+++ virtual_ssd_pkg/virtual_ssd.py
@@ -5,20 +5,22 @@ class VirtualSSD:
     def parsing_command(self, cmd):
         if type(cmd) == str:
             cmd_list = cmd.split(' ')
-            if len(cmd_list) == 3 and cmd_list[0:2] == ['ssd', 'R']:
-                if self.valid_LBA(cmd_list[2]):
-                    return "READ"
-                else:
-                    raise ValueError("INVALID COMMAND")
-            elif len(cmd_list) == 4 and cmd_list[0:2] == ['ssd', 'W']:
-                if self.valid_LBA(cmd_list[2]) and self.valid_value(cmd_list[3]):
-                    return "WRITE"
-                else:
-                    raise ValueError("INVALID COMMAND")
+            if self.is_valid_read_command(cmd_list):
+                return "READ"
+            elif self.is_valid_write_command(cmd_list):
+                return "WRITE"
             else:
                 raise ValueError("INVALID COMMAND")
         else:
             raise ValueError("INVALID COMMAND")
+
+    def is_valid_write_command(self, cmd_list):
+        return len(cmd_list) == 4 and cmd_list[0:2] == ['ssd', 'W'] and\
+            self.valid_LBA(cmd_list[2]) and self.valid_value(cmd_list[3])
+
+    def is_valid_read_command(self, cmd_list):
+        return len(cmd_list) == 3 and cmd_list[0:2] == ['ssd', 'R'] and \
+            self.valid_LBA(cmd_list[2])
 
     def valid_LBA(self, LBA):
         if LBA.isdigit():
