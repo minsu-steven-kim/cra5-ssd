3439702d91baf21ebeefedcd03b1e9bb4f71b134
Kidong.Cheon <k.cheon@samsung.com>
2024-06-18 14:09:48 +0900

    [Refactor] Testcase 코드 수정

--- shell_pkg/test_shell.py
+++ shell_pkg/test_shell.py
@@ -1,37 +1,35 @@
 import io
 import sys
 from unittest import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 from shell_pkg.shell import Shell
-
-NON_INIT_DATA = 0xAAAABBBB
 
 
 class TestShell(TestCase):
     def setUp(self):
         self.shell = Shell()
 
-    @patch.object(Shell, 'send_cmd_to_ssd')
-    def test_read_calling_send_cmd_to_ssd(self, mock):
+    def test_read_calling_send_cmd_to_ssd(self):
+        self.shell.send_cmd_to_ssd = Mock()
         self.shell.read(3)
         self.assertEqual(self.shell.send_cmd_to_ssd.call_count, 1)
 
-    @patch.object(Shell, 'get_result_with_ssd')
-    def test_read_calling_get_result_with_ssd(self, mock):
+    def test_read_calling_get_result_with_ssd(self):
         self.shell.get_result_with_ssd = Mock()
         self.shell.read(3)
         self.assertEqual(self.shell.get_result_with_ssd.call_count, 1)
 
-    @patch.object(Shell, 'get_result_with_ssd', return_value = NON_INIT_DATA)
-    def test_read_check_print_result(self, mock):
+    def test_read_check_print_result(self):
         output = io.StringIO()
         original_stdout = sys.stdout
         sys.stdout = output
         try:
+            self.shell.get_result_with_ssd = Mock()
+            self.shell.get_result_with_ssd.return_value = 0xAAAABBBB
             self.shell.read(3)
         finally:
             sys.stdout = original_stdout
 
         captured_output = int(output.getvalue().strip())
-        self.assertEqual(captured_output, NON_INIT_DATA)
+        self.assertEqual(captured_output, 0xAAAABBBB)
