46105396c658f17f449d820efc905d1045934235
dasol.han <koda911101@gmail.com>
2024-06-18 15:08:21 +0900

    [feature] mock file을 사용해서 read / write test 코드

--- virtual_ssd_pkg/test_virtual_ssd.py
+++ virtual_ssd_pkg/test_virtual_ssd.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
+from unittest.mock import mock_open, patch
 
 from virtual_ssd_pkg.ssd import VirtualSSD
+
 
 class TestVirtualSSD(TestCase):
     def setUp(self):
@@ -69,4 +71,24 @@ class TestVirtualSSD(TestCase):
     def test_write_called(self):
         self.assertEqual("WRITE", self.virtual_ssd.parsing_command("ssd W 10 0x10000000"))
 
+    @patch('builtins.open', new_callable=mock_open, read_data='0x00000000\n0x11111111\n0x22222222\n')
+    def test_read_with_mocked_file(self, mock_file):
+        def read_file_contents(filename, index):
+            with open(filename, 'r') as f:
+                lines = f.readlines()
+                if 0 <= index < len(lines):
+                    return lines[index].strip()
 
+                return None
+
+        # read test
+        result_read = read_file_contents('nand.txt', 1)
+        self.assertEqual(result_read, '0x11111111')
+        mock_file.assert_called_once_with('nand.txt', 'r')
+
+        # write test
+        with open('result.txt', 'w') as f:
+            f.write(result_read)
+
+        mock_file.assert_any_call('result.txt', 'w')  # 파일 쓰기도 mock 확인
+        mock_file.return_value.write.assert_called_once_with(result_read)
