c22cde927b7f317b8bbe22cb4f5d77639748f445
km4.kim <kyoomok@naver.com>
2024-06-18 12:03:40 +0900

    [refactoring] lba 체크 코드 리팩토링

--- shell_pkg/shell.py
+++ shell_pkg/shell.py
@@ -1,11 +1,17 @@
 import os
 
+
 class Shell:
     def __init__(self):
         pass
 
     def write(self, lba: int, value: str):
+        if self.is_invalid_lba(lba):
+            raise Exception("INVALID COMMAND")
+
+    def is_invalid_lba(self, lba):
         if type(lba) != int:
-            raise Exception("INVALID COMMAND")
+            return True
         if lba < 0 or lba > 99:
-            raise Exception("INVALID COMMAND")
+            return True
+        return False
