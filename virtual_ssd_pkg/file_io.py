401a9db36521c6051dfb2156774d64a0b75b74ae
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-18 14:49:21 +0900

    [feature] 파일 입출력 명세 변경에 따른 기능 재구현

--- virtual_ssd_pkg/file_io.py
+++ virtual_ssd_pkg/file_io.py
@@ -1,44 +1,14 @@
-INITIAL_VALUE = 0
-ARRAY_SIZE = 100
-NAND_FILENAME = f'{__file__}/../nand.txt'
-
-
 class FileIO:
-    def __init__(self):
-        self.array = [INITIAL_VALUE for _ in range(ARRAY_SIZE)]
-        self.load()
+    def __init__(self, filename):
+        self.filename = filename
 
     def load(self):
         try:
-            with open(NAND_FILENAME, 'rt') as f:
-                for i, line in enumerate(f.readlines()):
-                    self.array[i] = int(line.strip(), 16)
+            with open(self.filename, 'r') as f:
+                return f.read()
         except FileNotFoundError:
-            pass
+            return ''
 
-    def save(self):
-        with open(NAND_FILENAME, 'wt') as f:
-            for value in self.array:
-                f.write(format(value, '08x'))
-                f.write('\n')
-
-    def read(self, lba):
-        return self.array[lba]
-
-    def write(self, lba, data):
-        self.array[lba] = data
-        self.save()
-
-
-if __name__ == '__main__':
-    io = FileIO()
-    io.load()
-    print(io.array)
-    io.write(10, 0x12345678)
-    io.save()
-    io.load()
-    print(io.array)
-    io.write(10, 0xFFFFFFFF)
-    io.save()
-    io.load()
-    print(io.array)
+    def save(self, data):
+        with open(self.filename, 'w') as f:
+            f.write(data)
--- virtual_ssd_pkg/test_file_io.py
+++ virtual_ssd_pkg/test_file_io.py
@@ -1,30 +1,11 @@
 from unittest import TestCase
-
-from virtual_ssd_pkg.file_io import FileIO, ARRAY_SIZE
-
-TARGET_LBA = 10
-FAKE_DATA = 0x12345678
-FAKE_DATA_SMALLEST = 0x00000000
-FAKE_DATA_BIGGEST = 0xFFFFFFFF
+from virtual_ssd_pkg.file_io import FileIO
 
 
 class TestFileIO(TestCase):
     def setUp(self):
         super().setUp()
-        self.io = FileIO()
+        self.io = FileIO('virtual_ssd_pkg/nand.txt')
 
     def test_initialize(self):
-        self.assertIsNotNone(self.io.array)
-        self.assertEqual(ARRAY_SIZE, len(self.io.array))
-
-    def test_write_then_read(self):
-        self.io.write(TARGET_LBA, FAKE_DATA)
-        self.assertEqual(FAKE_DATA, self.io.read(TARGET_LBA))
-
-    def test_write_then_read_smallest(self):
-        self.io.write(TARGET_LBA, FAKE_DATA_SMALLEST)
-        self.assertEqual(FAKE_DATA_SMALLEST, self.io.read(TARGET_LBA))
-
-    def test_write_then_read_biggest(self):
-        self.io.write(TARGET_LBA, FAKE_DATA_BIGGEST)
-        self.assertEqual(FAKE_DATA_BIGGEST, self.io.read(TARGET_LBA))
+        self.assertEqual('virtual_ssd_pkg/nand.txt', self.io.filename)
