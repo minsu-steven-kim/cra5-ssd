0d140d13efc20b6e950351313901386557fc62d8
jihye <jhlovv08@gmail.com>
2024-06-18 12:00:17 +0900

    [feature] command length valid check 구현

--- virtual_ssd_pkg/test_virtual_ssd.py
+++ virtual_ssd_pkg/test_virtual_ssd.py
@@ -1,6 +1,32 @@
 from unittest import TestCase
 
+from virtual_ssd import VirtualSSD
 
 class TestVirtualSSD(TestCase):
+    def setUp(self):
+        self.virtual_ssd = VirtualSSD()
+
     def test_placeholder(self):
         self.assertEqual(1, 1)
+
+    def test_command_not_string(self):
+        with self.assertRaises(ValueError) as context:
+            self.virtual_ssd.parsing_command(123)
+
+        self.assertEqual(str(context.exception), "INVALID COMMAND")
+
+
+    def test_read_command_invalid_string(self):
+        with self.assertRaises(ValueError) as context:
+            self.virtual_ssd.parsing_command("ssd R 1234 123")
+
+        self.assertEqual(str(context.exception), "INVALID COMMAND")
+
+    def test_write_command_invalid_string(self):
+        with self.assertRaises(ValueError) as context:
+            self.virtual_ssd.parsing_command("ssd W 1234")
+
+        self.assertEqual(str(context.exception), "INVALID COMMAND")
+
+
+
--- virtual_ssd_pkg/virtual_ssd.py
+++ virtual_ssd_pkg/virtual_ssd.py
@@ -1,3 +1,17 @@
 class VirtualSSD:
     def __init__(self):
         pass
+
+    def parsing_command(self, cmd):
+        if type(cmd) == str:
+            cmd_list = cmd.split(' ')
+            if len(cmd_list) == 3 and cmd_list[0:2] == ['ssd', 'R']:
+                pass
+            elif len(cmd_list) == 4 and cmd_list[0:2] == ['ssd', 'W']:
+                pass
+            else:
+                raise ValueError("INVALID COMMAND")
+        else:
+            raise ValueError("INVALID COMMAND")
+
+
