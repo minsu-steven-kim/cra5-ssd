0e981d371e8690d677986f6ac1db37118d2ffe1f
Ho <ho@CRA.kr>
2024-06-19 10:45:26 +0900

    [feature] ssd write - LBA 0

--- virtual_ssd_pkg/test_command.py
+++ virtual_ssd_pkg/test_command.py
@@ -10,19 +10,27 @@ class TestCommand(TestCase):
         self.command = WriteCommand()
 
     def test_write_command_invalid_text(self):
-        args = 'ssd W 14234 4'
+        args = ['ssd','W','14234','4']
 
         with self.assertRaises(Exception):
             self.command.execute(args)
 
     def test_write_command_invalid_lba(self):
-        args = 'ssd W 14234 0x00000000'
+        args = ['W','14234','0x00000000']
 
         with self.assertRaises(Exception):
             self.command.execute(args)
 
     def test_write_command_invalid_data(self):
-        args = 'ssd W 0 0000000000'
+        args = ['W','1','0000000000']
 
         with self.assertRaises(Exception):
             self.command.execute(args)
+
+    def test_write_command_LBA_0(self):
+        FAKE_LBA = 0
+        args = ['W',str(FAKE_LBA),FAKE_DATA]
+        self.command.execute(args)
+        expected = self.command.NAND_TXT.load()[FAKE_LBA * line_len:(FAKE_LBA + 1) * line_len - 1]
+        self.assertEqual(expected, FAKE_DATA)
+
