57035bd19b370d67f95a102069160045bd973206
Ho <ho@CRA.kr>
2024-06-19 10:40:15 +0900

    [refactoring] ssd write - invalid lba

--- virtual_ssd_pkg/test_command.py
+++ virtual_ssd_pkg/test_command.py
@@ -7,18 +7,16 @@ line_len = 11
 
 class TestCommand(TestCase):
     def setUp(self):
-        pass
+        self.command = WriteCommand()
 
     def test_write_command_invalid_text(self):
         args = 'ssd W 14234 4'
-        command = WriteCommand()
 
         with self.assertRaises(Exception):
-            command.execute(args)
+            self.command.execute(args)
 
     def test_write_command_invalid_lba(self):
-        command = WriteCommand()
         args = 'ssd W 14234 0x00000000'
 
         with self.assertRaises(Exception):
-            command.execute(args)
+            self.command.execute(args)
