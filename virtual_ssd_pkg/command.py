58ce463dbb04548fd02d98aa69ce07252406e49f
Ho <ho@CRA.kr>
2024-06-19 10:34:03 +0900

    [feature] ssd write - invalid args

--- virtual_ssd_pkg/command.py
+++ virtual_ssd_pkg/command.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import re
+
+from virtual_ssd_pkg.file_io import FileIO
 
 
 class Command(ABC):
@@ -6,10 +9,45 @@ class Command(ABC):
     def execute(self, args):
         pass
 
+    def is_invalid_lba(self, lba: str):
+        if type(lba) != str:
+            return True
+        if len(lba) == 0:
+            return True
+        if not lba.isdigit():
+            return True
+        if 0 > int(lba) or int(lba) > 99:
+            return True
+        return False
+
+    def is_invalid_value(self, value: str):
+        if type(value) != str:
+            return True
+        return not bool(re.match(r'0x[0-9A-F]{8}$', value))
+
 
 class WriteCommand(Command):
-    def execute(self, args):
+    def __init__(self):
         pass
+
+    def is_invalid_parameter(self, args):
+        if len(args) < 3:
+            return True
+        if self.is_invalid_lba(args[1]):
+            return True
+        if self.is_invalid_value(args[2]):
+            return True
+        return False
+
+    def execute(self, args):
+        if self.is_invalid_parameter(args):
+            raise Exception("INVALID COMMAND")
+        self.NAND_TXT = FileIO("nand.txt")
+        self.NAND_DATA = self.NAND_TXT.load()
+        lba = int(args[1])
+        data = args[2]
+        self.NAND_DATA = self.NAND_DATA[:lba * 11] + data + self.NAND_DATA[(lba + 1) * 11 - 1:]
+        self.NAND_TXT.save(self.NAND_DATA)
 
 
 class ReadCommand(Command):
@@ -20,4 +58,4 @@ class ReadCommand(Command):
 class InvalidCommand(Command):
 
     def execute(self, args):
-        print('INVALID COMMAND')
+        raise Exception("INVALID COMMAND")
---
+++ virtual_ssd_pkg/test_command.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from virtual_ssd_pkg.command import WriteCommand
+
+FAKE_DATA = '0xBAADEEEE'
+line_len = 11
+
+class TestCommand(TestCase):
+    def setUp(self):
+        pass
+
+    def test_write_command_invalid_text(self):
+        args = 'ssd W 14234 4'
+        command = WriteCommand()
+
+        with self.assertRaises(Exception):
+            command.execute(args)
