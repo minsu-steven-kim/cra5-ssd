79ec1dcd66b1e87f9ebbdf66d91617db21dc08e5
dasol.han <koda911101@gmail.com>
2024-06-19 11:55:56 +0900

    [refactoring] ssd.py runtime error 개선 및 read test code 수정

--- virtual_ssd_pkg/test_virtual_ssd.py
+++ virtual_ssd_pkg/test_virtual_ssd.py
@@ -1,34 +1,34 @@
 import os
+import subprocess
 from unittest import TestCase
 from unittest.mock import patch
 
-from virtual_ssd_pkg.ssd import VirtualSSD
+from ssd import VirtualSSD
+from file_io import FileIO
 
+INVALID_COMMAND = "INVALID COMMAND"
 
 class TestVirtualSSD(TestCase):
     def setUp(self):
         self.virtual_ssd = VirtualSSD()
 
-    @patch('os.system')
-    def test_cli_invalid_command(self, mock_system):
-        mock_system.return_value = 1
-        result = os.system('python ssd.py A')
-        self.assertEqual(result, 1)
+    def test_cli_invalid_command(self):
+        result = subprocess.run(['python', 'ssd.py', 'A'], capture_output=True, text=True)
+        self.assertIn(INVALID_COMMAND, result.stdout)
 
-    @patch('os.system')
-    def test_read_with_cli(self, mock_system):
-        mock_system.return_value = 0
-        result = os.system('python ssd.py R 1')
-        self.assertEqual(result, 0)
+    def test_read_with_cli(self):
+        result = subprocess.run(['python', 'ssd.py', 'R', '1'], capture_output=True, text=True)
+        self.assertIn('', result.stdout)
+        file_io = FileIO('result.txt')
+        ret = file_io.load()
+        self.assertEqual(ret, '0x00000000')
 
-    @patch('os.system')
-    def test_read_with_cli_invalid_lba_range(self, mock_system):
-        mock_system.return_value = 1
-        result = os.system('python ssd.py R -1')
-        self.assertEqual(result, 1)
+    def test_read_with_cli_invalid_lba_range(self):
+        result = subprocess.run(['python', 'ssd.py', 'R', '-1'], capture_output=True, text=True)
+        self.assertIn(INVALID_COMMAND, result.stdout)
 
-        result = os.system('python ssd.py R 100')
-        self.assertEqual(result, 1)
+        result = subprocess.run(['python', 'ssd.py', 'R', '100'], capture_output=True, text=True)
+        self.assertIn(INVALID_COMMAND, result.stdout)
 
     @patch('os.system')
     def test_write_with_cli(self, mock_system):
