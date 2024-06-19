a5a70f0ead82ea530945a9488147e8ccccb26720
jihye <jhlovv08@gmail.com>
2024-06-19 09:24:31 +0900

    [feature] shell help command 구현

--- shell_pkg/command.py
+++ shell_pkg/command.py
@@ -11,6 +11,26 @@ class ExitCommand(Command):
     def execute(self):
         return 1
 
+class HelpCommand(Command):
+    def execute(self):
+        print("""============================== Command Guide ==============================
+1) write [LBA] [value]
+: write [value] to [LBA].
+: [LBA] should be an integer between 0 and 99.
+: [value] should be in hexadecimal format between 0x00000000 and 0xFFFFFFFF.
+2) read [LBA]
+: read the value written to [LBA].
+3) fullwrite [value]
+: write [value] to all LBA(0~99).
+: [value] should be in hexadecimal format between 0x00000000 and 0xFFFFFFFF.
+4) fullread
+: read the all LBA(0~99) values.
+5) exit
+: quit the shell.
+6) help
+: see the command guide.""")
+        return 0
+
 
 class InvalidCommand(Command):
     def execute(self):
