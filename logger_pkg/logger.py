638f5244b9a0cdd989bc0f0f319c39a279018550
Kidong.Cheon <k.cheon@samsung.com>
2024-06-20 14:25:28 +0900

    [Refactor] 화면에 출력하는 동작 수정

--- logger_pkg/logger.py
+++ logger_pkg/logger.py
@@ -10,7 +10,7 @@ class Logger:
         method_name = 'main' if __name__ == 'logger_pkg.logger' else inspect.currentframe().f_back.f_code.co_name
         formmatedMathod = f'{self.__class__.__name__}.{method_name}()'
         fomattedLog = f'[{dateFormat}] {formmatedMathod:<30} : {log}'
-        self.displayLog(fomattedLog)
+        self.displayLog(log)
 
         filePath = './latest.log'
         newPath = f'until_{now.strftime("%y%m%d_%Hh_%Mm_%Ss")}.zip'
