4f9c082e17c7648373a2cbe12a7ed677d5dca4f1
km4.kim <kyoomok@naver.com>
2024-06-20 12:13:22 +0900

    [feature] buffer.txt 파일 쓰기 구현

--- virtual_ssd_pkg/buffer_manager.py
+++ virtual_ssd_pkg/buffer_manager.py
@@ -1,18 +1,33 @@
-from virtual_ssd_pkg.constants import BUFFER_FILE_PATH
+import os.path
+
+from command import FlushCommand
+from constants import BUFFER_FILE_PATH, MAX_CMD_BUFFER
 
 
 class BufferManager:
     def __init__(self):
         pass
 
-    def insert_cmd_to_buffer(self, cmd):
-        # TO-DO: when max buffer
-        # cmd_list = []
-        # with open(BUFFER_FILE_PATH, 'r') as file:
-        #     for line in file:
-        #         cmd_list.append(line)
-        # if line >= MAX_CMD_BUFFER:
-        #     return FlushCommand()
+    def run_flush_command(self):
+        FlushCommand("F").execute()
 
-        with open(BUFFER_FILE_PATH, 'w') as file:
-            file.write(cmd)
+    def list_to_str(self, list_value):
+        return ' '.join(map(str, list_value))
+
+    def run(self, args):
+        cmd_list = []
+        is_flush = False
+        if os.path.exists(BUFFER_FILE_PATH):
+            with open(BUFFER_FILE_PATH, 'r') as f:
+                args_list = f.readlines()
+                for line in args_list:
+                    cmd_list.append(line)
+
+                if len(cmd_list) >= MAX_CMD_BUFFER:
+                    self.run_flush_command()
+                    is_flush = True
+
+        with open(BUFFER_FILE_PATH, 'a') as f:
+            if is_flush:
+                f.truncate(0)
+            f.write(self.list_to_str(args) + "\n")
--- virtual_ssd_pkg/ssd.py
+++ virtual_ssd_pkg/ssd.py
@@ -2,16 +2,18 @@ import sys
 
 from command import CommandFactory
 from constants import INVALID_COMMAND
-from virtual_ssd_pkg.buffer_manager import BufferManager
+from buffer_manager import BufferManager
 
 
 class VirtualSSD:
     def __init__(self):
         self.bm = BufferManager()
         self.command_factory = CommandFactory()
+
     def run(self):
         args = sys.argv[1:]
         cmd = self.command_factory.create_command(args)
+        self.bm.run(args)
         cmd.execute(args)
 
 if __name__ == '__main__':
